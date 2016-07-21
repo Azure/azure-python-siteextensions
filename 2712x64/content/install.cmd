868039bc40d1b09610eabe6d79d8f6bfb8fb0ac8
Steve Dower <steve.dower@microsoft.com>
2016-07-21 13:44:16 -0700

    Removes unnecessary environment variable. Fixes installation when an equivalent site extension already exists. Adds 32-bit extensions.

--- 2712x64/content/install.cmd
+++ 2712x64/content/install.cmd
@@ -1,1 +1,3 @@
+taskkill /F /IM python.exe
+if exist D:\home\Python27 rmdir /Q /S D:\home\Python27
 xcopy /s Python27 D:\home\Python27\
--- build_all.bat
+++ build_all.bat
@@ -2,4 +2,6 @@
 @set D=%~dp0
 
 @call "%D%\build.bat" "py -3.5" Python35 352x64
+@call "%D%\build.bat" "py -3.5-32" Python35 352x86
 @call "%D%\build.bat" "py -2.7" Python27 2712x64
+@call "%D%\build.bat" "py -2.7-32" Python27 2712x86
