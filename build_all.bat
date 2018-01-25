793a9984c116a1882d4a91251bd41c5521110da8
Steve Dower <steve.dower@microsoft.com>
2018-01-26 09:26:11 +1100

    Removes archived versions from build script

--- build_all.bat
+++ build_all.bat
@@ -3,23 +3,13 @@
 
 @call "%D%\build.bat" python     3.6.4 364x64
 @call "%D%\build.bat" python     3.6.2 362x64
-@call "%D%\build.bat" python     3.6.1 361x64
-@call "%D%\build.bat" python     3.6.0 360x64
 @call "%D%\build.bat" python     3.5.4 354x64
 @call "%D%\build.bat" python     3.5.3 353x64
-@call "%D%\build.bat" python     3.5.2 352x64
 
 @call "%D%\build.bat" pythonx86  3.6.4 364x86
 @call "%D%\build.bat" pythonx86  3.6.2 362x86
-@call "%D%\build.bat" pythonx86  3.6.1 361x86
-@call "%D%\build.bat" pythonx86  3.6.0 360x86
 @call "%D%\build.bat" pythonx86  3.5.4 354x86
 @call "%D%\build.bat" pythonx86  3.5.3 353x86
-@call "%D%\build.bat" pythonx86  3.5.2 352x86
 
 @call "%D%\build.bat" python2    2.7.14 2714x64
-@call "%D%\build.bat" python2    2.7.13 2713x64
-@call "%D%\build.bat" python2    2.7.12 2712x64
 @call "%D%\build.bat" python2x86 2.7.14 2714x86
-@call "%D%\build.bat" python2x86 2.7.13 2713x86
-@call "%D%\build.bat" python2x86 2.7.12 2712x86
