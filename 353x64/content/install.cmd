db2e1d0b0a58c2019b86680db9a2b0e780c46597
Steve Dower <steve.dower@microsoft.com>
2018-02-08 14:54:28 -0800

    Undo fix for #18 as Azure now unescapes filenames on extract.

--- 353x64/content/install.cmd
+++ 353x64/content/install.cmd
@@ -11,8 +11,3 @@ if not exist %SRC% (
 taskkill /F /IM python.exe
 if exist D:\home\%SRC% rmdir /Q /S D:\home\%SRC%
 move %SRC% D:\home\%SRC%
-
-if exist D:\home\%SRC% (
-    ren D:\home\%SRC%\Lib\site-packages\setuptools\script%%%%20(dev).tmpl "script (dev).tmpl"
-    ren D:\home\%SRC%\Lib\site-packages\setuptools\launcher%%%%20manifest.xml "launcher manifest.xml"
-)
