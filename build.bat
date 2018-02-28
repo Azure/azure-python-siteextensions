d1f77cb56de8ddd8885834a15236fd7fd0b5176b
Steve Dower <steve.dower@microsoft.com>
2018-02-28 13:30:50 -0800

    Fix missing backslashes

--- build.bat
+++ build.bat
@@ -6,19 +6,19 @@
 @set NUGET="%D%Tools\nuget.exe"
 @set CONTENT=%OUTDIR%\%~3\content\python%~3
 
-%NUGET% install %1 -Version %2 -OutputDirectory "%OUTDIR%source_packages"
-@set PY="%OUTDIR%source_packages\%~1.%~2\tools\python.exe"
+%NUGET% install %1 -Version %2 -OutputDirectory "%OUTDIR%\source_packages"
+@set PY="%OUTDIR%\source_packages\%~1.%~2\tools\python.exe"
 
 @rem For older versions of Python, overwrite bdist_wininst to allow setuptools
 @rem to execute its install_scripts command without crashing.
-copy /Y "%D%Tools\distutils.command.bdist_wininst.py" "%OUTDIR%source_packages\%~1.%~2\tools\Lib\distutils\command\bdist_wininst.py"
+copy /Y "%D%Tools\distutils.command.bdist_wininst.py" "%OUTDIR%\source_packages\%~1.%~2\tools\Lib\distutils\command\bdist_wininst.py"
 
 @rmdir /q /s "%CONTENT%" 2> nul
 %PY% -m pip install -U pip setuptools certifi wfastcgi
 %PY% "%D%Tools\copy_py.py" %3 "%CONTENT%\\"
 @if errorlevel 1 exit /B
-@if not exist "%OUTDIR%packages" mkdir "%OUTDIR%packages"
-%NUGET% pack -NoPackageAnalysis "%D%%3\python%~3.nuspec" -OutputDirectory "%OUTDIR%packages"
+@if not exist "%OUTDIR%\packages" mkdir "%OUTDIR%\packages"
+%NUGET% pack -NoPackageAnalysis "%D%%3\python%~3.nuspec" -OutputDirectory "%OUTDIR%\packages"
 @if not errorlevel 1 rmdir /q /s "%CONTENT%"
 @exit /B 0
 
